c8db4293004e2727b68a5d9e87e3fb74e896eb4d
JustGamingLTD <33322794+JustGamingLTD@users.noreply.github.com>
2019-01-02 13:16:59 +0100

    Update main.py

--- Reinforcement Learning/main.py
+++ Reinforcement Learning/main.py
@@ -58,3 +58,18 @@ class Memory:
         else:
             return random.sample(self._samples, no_samples)
         
+class GameRunner:
+    def __init__(self, sess, model, env, memory, max_eps, min_eps,
+                 decay, render=True):
+        self._sess = sess
+        self._env = env
+        self._model = model
+        self._memory = memory
+        self._render = render
+        self._max_eps = max_eps
+        self._min_eps = min_eps
+        self._decay = decay
+        self._eps = self._max_eps
+        self._steps = 0
+        self._reward_store = []
+        self._max_x_store = []
